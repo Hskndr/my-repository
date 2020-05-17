3d7f528b37b3f3dc23612ad8177c2c3f47a908d2
hiskander aguillon <hiskander@gmail.com>
2020-05-17 20:59:03 -0300

    add finance

--- hskndr-portfolio/src/app/app-routing.module.ts
+++ hskndr-portfolio/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent, AboutComponent } from './components';
 
 
 const routes: Routes = [
+  { path: 'finance', loadChildren: () => import('./modules/finance/finance.module').then(m => m.FinanceModule) },
+  { path: 'portfolio', loadChildren: () => import('./modules/portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: 'blog', loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule) },
+  { path: 'shopping', loadChildren: () => import('./modules/shopping/shopping.module').then(m => m.ShoppingModule) },
   { path: 'about', component: AboutComponent },
-  { path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
-  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-  { path: 'shopping', loadChildren: () => import('./shopping/shopping.module').then(m => m.ShoppingModule) },
   { path: 'home', component: HomeComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
